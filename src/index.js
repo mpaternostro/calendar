14989bcd3c905dd5c73f878c13dd1dc9f455cabe
mpaternostro <marcelodpaternostro@gmail.com>
2020-05-02 20:21:06 -0300

    soporte superposicion de eventos, agrega dependencias, modifica eventos de prueba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import test from '../data/index.js';
 import ManageUser from './ManageUser.js';
-import Weekly from './Weekly.js';
+import Weekly, { autoScroll } from './Weekly.js';
 
 // EMULA LA RESPUESTA DE LA API CON LOS USUARIOS EXISTENTES
 const users = [
@@ -24,13 +24,26 @@ const users = [
   },
 ];
 
-const dates = '28 mar - 3 apr';
+
+// const dates = [
+//   new Date(2020, 3, 28),
+//   new Date(2020, 3, 29),
+//   new Date(2020, 3, 30),
+//   new Date(2020, 3, 31),
+//   new Date(2020, 4, 1),
+//   new Date(2020, 4, 2),
+//   new Date(2020, 4, 3),
+// ];
+
+const date = new Date('2020-04-26T19:03:52.214Z');
+// const dates = '28 mar - 3 apr';
 const events = [test];
 
-const weekly = new Weekly('Marcelo', events, true, dates);
+const weekly = new Weekly('Marcelo', events, true, date);
 weekly.showTopBar();
 weekly.showCalendar();
 weekly.showEvents();
+autoScroll();
 
 const marcelo = new ManageUser(users);
 marcelo.showUsers();
