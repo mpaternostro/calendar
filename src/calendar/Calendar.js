72ffb596cd23de06d65a095c798365219057d325
mpaternostro <marcelodpaternostro@gmail.com>
2020-05-05 18:13:59 -0300

    refactor clase Weekly

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -1,7 +1,9 @@
+import sortEvents from './utilities.js';
+
 export default class Calendar {
   constructor(owner, events, showWeekends) {
     this.owner = owner;
-    this.events = events.sort((a, b) => a.start.getTime() - b.start.getTime());
+    this.events = sortEvents(events);
     this.showWeekends = showWeekends;
   }
 }
